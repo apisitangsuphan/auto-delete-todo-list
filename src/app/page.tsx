84326b4df71a4ee7576fe8246e036698b44484a0
Apisit Angsuphan <60085220+sussus003@users.noreply.github.com>
2025-02-27 10:55:48 +0700

    Its almost like the prototype

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,60 +7,88 @@ export default function Home() {
   const [sumData, setSumData] = useState<typeData[]>(sumDataList || []);
   const [fruitList, setFruitList] = useState<typeData[]>([]);
   const [vegetableList, setVegetableList] = useState<typeData[]>([]);
-  
-  useEffect(() => {
-    const timers = [...fruitList,...vegetableList].map(item => 
-      setTimeout( () => moveToMainList(item),5000)
-    )
-    return () => timers.forEach(timer => clearTimeout(timer))
-  },[fruitList,vegetableList])
 
-  const moveToMainList = (item:typeData) => {
-    if(item.type === "Fruit"){
-      setFruitList(fruitList => fruitList.filter(fruit => fruit.name !== item.name));
-    }else{
-      setVegetableList(vegetableList => vegetableList.filter(veg => veg.name !== item.name))
+  useEffect(() => {
+    const timers = [...fruitList, ...vegetableList].map((item) =>
+      setTimeout(() => moveToMainList(item), 1000)
+    );
+    return () => timers.forEach((timer) => clearTimeout(timer));
+  }, [fruitList, vegetableList]);
+
+  const moveToMainList = (item: typeData) => {
+    if (item.type === "Fruit") {
+      setFruitList((fruitList) =>
+        fruitList.filter((fruit) => fruit.name !== item.name)
+      );
+    } else {
+      setVegetableList((vegetableList) =>
+        vegetableList.filter((veg) => veg.name !== item.name)
+      );
     }
-    setSumData((prev => [...prev,item]));
-  }
+    setSumData((prev) => [...prev, item]);
+  };
 
   const handleColumnClick = (item: typeData) => {
-    if(item.type === "Fruit"){
-      setFruitList(fruitList => [...fruitList,item]);
-    }else{
-      setVegetableList(vegetableList => [...vegetableList,item])
+    if (item.type === "Fruit") {
+      setFruitList((fruitList) => [...fruitList, item]);
+    } else {
+      setVegetableList((vegetableList) => [...vegetableList, item]);
     }
-    setSumData(sumData => sumData.filter(sumData => sumData.name !== item.name));
+    setSumData((sumData) =>
+      sumData.filter((sumData) => sumData.name !== item.name)
+    );
   };
 
   return (
-    <div className="container mt-20 mx-auto justify-center">
-      <div className="grid grid-cols-3 gap-4 text-center mx-5 max-w-[1000px]">
-        <div className="main border-2 border-slate-500 h-[500px]">
+    <div className="container mt-20 mx-auto">
+      <div className="grid grid-cols-3 justify-center gap-4 text-center mx-5 max-w-[800px] h-[500px]">
+        <div className="main flex flex-col">
           {sumData.map((item) => {
-            return (<button type="button" key={item.name} onClick={() => {}}>
-              {item.name}
+            return (
+              <button
+                type="button"
+                key={item.name}
+                onClick={() => handleColumnClick(item)}
+                className="border-2 border-slate-100 mb-1 hover:bg-slate-100 hover:cursor-pointer"
+              >
+                {item.name}
               </button>
             );
           })}
         </div>
-        <div className="fruit border-2 border-slate-200 h-[500px]">
-          <h1 className="bg-slate-200">Fruit</h1>
-          {fruitList.map((item) => {
-            return (<button type="button" key={item.name} onClick={() => handleColumnClick(item)}>
-              {item.name}
-              </button>
-            );
-          })}
+        <div className="fruit border-2 border-slate-100 h-[500px]">
+          <div className=" flex flex-col">
+            <h1 className="bg-slate-100 mb-1">Fruit</h1>
+            {fruitList.map((item) => {
+              return (
+                <button
+                  type="button"
+                  key={item.name}
+                  onClick={() => moveToMainList(item)}
+                  className="border-2 border-slate-100 mb-1 mx-1 hover:bg-slate-100 hover:cursor-pointer"
+                >
+                  {item.name}
+                </button>
+              );
+            })}
+          </div>
         </div>
-        <div className="vegetable border-2 border-slate-200">
-        <h1 className="bg-slate-200">Vegetable</h1>
-        {vegetableList.map((item) => {
-            return (<button type="button" key={item.name} onClick={() => handleColumnClick(item)}>
-              {item.name}
-              </button>
-            );
-          })}
+        <div className="vegetable border-2 border-slate-100">
+          <div className=" flex flex-col">
+            <h1 className="bg-slate-100 mb-1">Vegetable</h1>
+            {vegetableList.map((item) => {
+              return (
+                <button
+                  type="button"
+                  key={item.name}
+                  onClick={() => moveToMainList(item)}
+                  className="border-2 border-slate-100 mb-1 mx-1 hover:bg-slate-100 hover:cursor-pointer"
+                >
+                  {item.name}
+                </button>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
